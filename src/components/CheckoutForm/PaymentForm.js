843a56a6cc964c344e646148275bbb518e9cadc9
orlandofat <orlandofat@gmail.com>
2021-03-18 12:23:54 -0400

    address form

--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Typography, Button, Divider} from "@material-ui/core"
+import {Elements, CardElement, ElementsConsumer } from "@stripe/react-stripe-js"
+import {loadStrips} from "@stripe/stripe-js"
+import Review from "./Review"
 
-const PaymentForm = () => {
+const PaymentForm = ({checkoutToken}) => {
     return (
-        <div>
-            kkk
-        </div>
+        <>
+            <Review checkoutToken={checkoutToken}/>
+            <Divider />
+            <Typography></Typography>
+        </>
     )
 }
 
